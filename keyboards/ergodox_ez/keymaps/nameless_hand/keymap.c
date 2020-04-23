e936800c9713b3c6f41c6265a1511add9e14b2f7
Jordan Ribera <jordanribera@gmail.com>
2020-04-23 15:41:16 -0700

    latest

--- keyboards/ergodox_ez/keymaps/nameless_hand/keymap.c
+++ keyboards/ergodox_ez/keymaps/nameless_hand/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   `    |   1  |   2  |   3  |   4  |   5  | PSCR |           | RIGHT|   6  |   7  |   8  |   9  |   0  | DELETE |
+ * |   `    |   1  |   2  |   3  |   4  |   5  | PSCR |           | <->  |   6  |   7  |   8  |   9  |   0  | DELETE |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | TAB    |   Q  |   W  |   E  |   R  |   T  | HYPER|           | MEH  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                           KC_PGDOWN,
                                                          KC_BSPC, KC_SPC, TT(NUMP),
   // right hand
-  KC_RGHT,      KC_6,    KC_7,    KC_8,     KC_9,    KC_0,       KC_DEL,
+  LCTL(KC_RCTL),KC_6,    KC_7,    KC_8,     KC_9,    KC_0,       KC_DEL,
   MEH_T(KC_NO), KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,       KC_BSLS,
                 KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN,    KC_QUOT,
   KC_EQL,       KC_N,    KC_M,    KC_COMM,  KC_DOT,  KC_SLSH,    KC_RSFT,
